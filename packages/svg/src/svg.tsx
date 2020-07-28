97520f9bd22060475e7038e7427ea43b514a8b9d
Ryosuke <ryosuke.san.hana@gmail.com>
2020-07-28 14:12:27 -0700

    refactor(svg): no need for separate props
    Removed duplicate props and consolidated use to the first prop.

--- packages/svg/src/svg.tsx
+++ packages/svg/src/svg.tsx
@@ -10,25 +10,19 @@ import {
 import { Box, Assign, BoxOwnProps } from "zenny-ui-box";
 
 export interface SVGProps
-  extends Assign<React.ComponentPropsWithRef<"svg">, BoxOwnProps> {
+  extends Assign<React.ComponentProps<"svg">, BoxOwnProps> {
   appearanceFill: AppearanceProp;
   appearanceColor: AppearanceProp;
 }
 
-export interface SVGSizeProps {
-  as: React.ReactNode;
-  width?: string;
-  height?: string;
-}
-
 export const SVG = styled(Box).attrs(
-  ({ as: Component, width, height }: SVGSizeProps) => ({
+  ({ as: Component, width, height, viewBox }: SVGProps) => ({
     // Define props on top of Box
     // Set underlying element as svg
     // Or custom component (like react-icons)
     as: Component ?? "svg",
     xmlns: "http://www.w3.org/2000/svg",
-    viewBox: "0 0 24 24",
+    viewBox: viewBox ?? "0 0 24 24",
     width: width ?? "24px",
     height: height ?? "24px",
   })
