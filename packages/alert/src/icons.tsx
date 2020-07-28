b1e455111e5cf68c894435b690314f9ec109494e
Ryosuke <ryosuke.san.hana@gmail.com>
2020-07-28 13:37:50 -0700

    fix(alert): use color prop instead of variant
    Since the alert box is already colored, this icon will generally be white (dep. on color contrast)

--- packages/alert/src/icons.tsx
+++ packages/alert/src/icons.tsx
@@ -7,11 +7,7 @@ import {
 } from "react-icons/fi";
 import { SVG } from "zenny-ui-svg";
 
-export const DangerIcon = () => <SVG as={FiXOctagon} appearance="danger" />;
-export const WarningIcon = () => <SVG as={FiFlag} strokeColor="warning" />;
-export const MessageIcon = () => (
-  <SVG as={FiMessageCircle} strokeColor="message" />
-);
-export const SuccessIcon = () => (
-  <SVG as={FiCheckCircle} strokeColor="success" />
-);
+export const DangerIcon = () => <SVG as={FiXOctagon} color="white" />;
+export const WarningIcon = () => <SVG as={FiFlag} color="white" />;
+export const MessageIcon = () => <SVG as={FiMessageCircle} color="white" />;
+export const SuccessIcon = () => <SVG as={FiCheckCircle} color="white" />;
