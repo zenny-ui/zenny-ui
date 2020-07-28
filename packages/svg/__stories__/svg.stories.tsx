4f23ae5e98c119e85ae1cb95cc64a7f819a790ee
Ryosuke <ryosuke.san.hana@gmail.com>
2020-07-28 13:35:35 -0700

    test(svg): test different color styling options
    Displaying all color styling options for icons, from new variants to themed colors to direct colors.

--- packages/svg/__stories__/svg.stories.tsx
+++ packages/svg/__stories__/svg.stories.tsx
@@ -19,12 +19,31 @@ export const HardcodedSVG = () => (
 // Pass any React SVG component to wrap it in our theming utilities
 export const ReactIcons = () => <SVG as={FiArrowDownCircle} />;
 
-// Use `appearance` prop to color icon "fill"
+// Use `appearance` prop to theme icon "fill" with brand/state variants
 export const ReactIconsAppearance = () => (
-  <SVG as={FiArrowDownCircle} appearance="primary" />
+  <SVG as={FiArrowDownCircle} appearanceFill="primary" />
 );
 
-// Use `strokeColor` prop to color icon stroke
+// Use `appearanceColor` prop to theme icon stroke with brand/state variants
+export const ReactIconsAppearanceStroke = () => (
+  <SVG as={FiArrowDownCircle} appearanceColor="primary" />
+);
+
+// Use `color` prop (combined with fill as currentcolor)
+// to theme icon stroke or color with theme tokens
+export const ReactIconsThemedFill = () => (
+  <SVG as={FiArrowDownCircle} color="primary" fill="currentcolor" />
+);
+export const ReactIconsThemedStroke = () => (
+  <SVG as={FiArrowDownCircle} color="primary" stroke="currentcolor" />
+);
+
+// Use `fill` prop to color icon fill with unthemed, CSS properties
+export const ReactIconsFillColor = () => (
+  <SVG as={FiArrowDownCircle} fill="#384930" />
+);
+
+// Use `stroke` prop to color icon stroke with unthemed, CSS properties
 export const ReactIconsStrokeColor = () => (
-  <SVG as={FiArrowDownCircle} strokeColor="primary" />
+  <SVG as={FiArrowDownCircle} stroke="#984930" />
 );
